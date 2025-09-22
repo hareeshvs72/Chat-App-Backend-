207375340e3b1373d2ad6672f77c14995a99ff6c
hareeshvs72 <hareeshvs72@gmail.com>
2025-09-22 17:58:56 +0530

    chat app back end

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on('message', (message) => {
+        console.log("Message received:", message);
+        io.emit("message", message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
