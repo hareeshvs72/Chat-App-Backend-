ebd07501cb89e9ef48b70e6e29cd290ac6406205
hareeshvs72 <hareeshvs72@gmail.com>
2025-09-22 19:23:56 +0530

    url updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:5173",
-            "https://your-frontend-app.onrender.com" // Your frontend URL later
+            "https://chat-app-front-end-khaki.vercel.app" // Your frontend URL later
         ],
         methods: ["GET", "POST"]
     }
